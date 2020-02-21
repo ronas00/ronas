b722c49486e4ee38782365082c90ce7d22d55e28
elias <elias.tl1278@gmail.com>
2020-02-21 11:04:41 +0330

    add login and sign up

--- src/components/login.js
+++ src/components/login.js
@@ -1,33 +1,74 @@
 import React, { Component } from "react";
-
+import Axios from "axios"
 
 export default class Login extends Component {
+    state = {
+        info: "",
+        field: {
+            username: "",
+            password: "",
+        },
+        errors: []
+    }
+
+
+
+
+    serndRequest = async () => {
+        Axios.post("http://176.9.164.199:3004/auth/login", {
+            "username": this.state.field.username,
+            "password": this.state.field.password
+        })
+    }
+    makeRequest() {
+        if (this.state.errors.length == 0) {
+            this.serndRequest()
+                .then(res=>{
+                })
+        }
+
+    }
+    handleChange(event) {
+        let field = this.state.field;
+        const target = event.target;
+        field[target.name] = target.value;
+        this.setState({ field });
+    }
+    chekValiadtion() {
+        console.log("heloo")
+        const { username, password, confirmpassword } = this.state.field
+        let errors = {}
+        if (username.length == 0) {
+            errors["username"] = "username is empty"
+        }
+        if (password.length == 0) {
+            errors["password"] = "password is empty"
+        }
+        if (confirmpassword.length == 0) {
+            errors["confirmpassword"] = "confirmpassword is empty"
+        }
+        if (confirmpassword !== password) {
+            errors["confirmpassword"] = "not same with password"
+        }
+        this.setState({ errors, loading: true })
+    }
     render() {
+        let {errors}=this.state
         return (
-            <form>
-                <h3>Log In</h3>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            <form className="background-main d-flex w-100 h-100 align-items-center">
+                <div className="container-fluid main-login p-5" style={{ height: "300px", width: "300px" }}>
+                            <p>LOGIN</p>
+                    <div className="form-group">
+                        <input type="text" name="username" className="form-control mb-2"
+                        onChange={this.handleChange.bind(this)} placeholder="username" />
+                        <input type="password" name="password" 
+                        onChange={this.handleChange.bind(this)} className="form-control mb-2" placeholder="password" />
+                        <button type="text" className="btn btn-primary btn-block btn-sm ">Submit</button>
+                        <p className="forgot-password text-left">
+                            <a href="#"> Forget password?</a>
+                        </p>
                     </div>
                 </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    <a href="#"> Forgot password?</a>
-                </p>
             </form>
         );
     }
