37c27d11fe6140c092231cc42951f3e4bb3168e6
ronas00 <ronasghalichebaf@gmail.com>
2020-02-21 12:32:42 +0330

    ...

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
 
 import Login from "./components/login";
 import SignUp from "./components/signup";
+import changePassword from "./components/changePassword";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route exact path='/' component={Login} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/changePassword" component={changePassword} />
       </Switch>
     </Router >
   );
